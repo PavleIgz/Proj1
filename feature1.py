dcbefdbba88ecf9c8d7091ca28d6aae87d7a43fe
PavleZx <pz011905656@fer.hr>
2024-02-22 23:15:00 +0100

    feature commit 1

---
+++ feature1.py
@@ -0,0 +1,39 @@
+import os, time
+import numpy as np
+import math as mt
+
+def reader():
+
+    flag  = False
+    n = 0
+    while (flag == False):
+        n = input("Unsite broj: ")
+        try:
+            n = int(n)
+            if (n == 0):
+                continue
+            flag = True
+        except:
+            pass
+            
+    sum = n
+    while(flag == True):
+        inp = input("Unesite broj: ")
+        try:
+            inp = int(inp)
+            if (inp != n):
+                break
+            sum += inp
+        except:
+            flag = False
+            pass
+        
+    return sum
+
+def main():
+    sum = reader()
+    print(sum)
+
+    return 0
+
+main()
